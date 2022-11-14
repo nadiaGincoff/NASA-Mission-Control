7bda9b59dc8b6cc69ce2522310071b688f1c109b
nadiaGincoff <nadia.gincoff@gmail.com>
2022-11-14 16:35:58 -0300

    modified launches test for integration with mongoDB

--- server/src/server.js
+++ server/src/server.js
@@ -1,26 +1,17 @@
 const http = require('http');
-const mongoose = require('mongoose');
 
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
-const PORT = process.env.PORT || 8000;
+const { mongoConnect } = require('./services/mongo');
 
-const MONGO_URL = 'mongodb+srv://nadiaGincoff:RMjTz0pQLcZOqYsC@nasacluster.hzqyqgy.mongodb.net/?retryWrites=true&w=majority'
+const PORT = process.env.PORT 
 
 const server = http.createServer(app);
 
-mongoose.connection.once('open', () => {
-    console.log('MongoDB connection is ready!');
-});
-
-mongoose.connection.on('error', (error) => {
-    console.error(error);
-});
-
 async function startServer() {
-    await mongoose.connect(MONGO_URL);
+    await mongoConnect();
     await loadPlanetsData();
     server.listen(PORT, () => {
         console.log(`Listening on port ${PORT}`);
